b4d24c8ba54bab3e9465f76cddd5e2d81a3916ed
frx08 <frx089@gmail.com>
2018-03-12 21:59:11 +0100

    handle different schema configurations and prepare $ref support

--- jsonschema2popo.py
+++ jsonschema2popo.py
@@ -9,7 +9,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class JsonSchema2Popo(object):
     """Converts a JSON Schema to a Plain Old Python Object class"""
-    definitions = []
+    definitions = {}
 
     TEMPLATES_FOLDER = 'templates/'
     CLASS_TEMPLATE_FNAME = '_class.tmpl'
@@ -29,43 +29,61 @@ class JsonSchema2Popo(object):
 
     def load(self, json_schema_file):
         self.process(json.load(json_schema_file))
+        # replace $refs
 
     def process(self, json_schema):
         for _obj_name, _obj in json_schema['definitions'].items():
             model = {}
             model['name'] = _obj_name
+            if 'type' in _obj:
+                model['type'] = _obj['type']
+
             model['properties'] = []
             model['defaults'] = []
-            for _prop_name, _prop in _obj['properties'].items():
-                if 'default' in _prop:
-                    _prop_value = _prop['default']
-                    if _prop['type'] == 'string':
-                        _prop_value = "'{}'".format(_prop_value)
-                    model['defaults'].append({
+            if 'properties' in _obj:
+                for _prop_name, _prop in _obj['properties'].items():
+                    if 'default' in _prop:
+                        _prop_value = _prop['default']
+                        if _prop['type'] == 'string':
+                            _prop_value = "'{}'".format(_prop_value)
+                        model['defaults'].append({
+                            '_name': _prop_name,
+                            '_value': _prop_value
+                        })
+                    
+                    if 'type' in _prop and isinstance(_prop['type'], list) and len(_prop['type']) > 1:
+                        _type = None
+                    elif 'type' in _prop and isinstance(_prop['type'], list):
+                        _type = self.J2P_TYPES[_prop['type'][0]]
+                    elif 'type' in _prop and isinstance(_prop['type'], str):
+                        _type = self.J2P_TYPES[_prop['type']]
+                    elif 'allOf' in _prop:
+                        _type = None
+                    elif '$ref' in _prop:
+                        _type = None
+                    else:
+                        _type = None
+                    
+                    prop = {
                         '_name': _prop_name,
-                        '_value': _prop_value
-                    })
-                
-                prop = {
-                    '_name': _prop_name,
-                    '_type': self.J2P_TYPES[_prop['type']]
-                }
-                if 'enum' in _prop:
-                    prop['enum'] = {
-                        '_name': "%sTypes" % (_prop_name.capitalize(), ),
-                        '_values': ' '.join(_prop['enum'])
+                        '_type': _type
                     }
-                model['properties'].append(prop)
+                    if 'enum' in _prop:
+                        prop['enum'] = {
+                            '_name': "%sTypes" % (_prop_name.capitalize(), ),
+                            '_values': ' '.join(_prop['enum'])
+                        }
+                    model['properties'].append(prop)
 
-            model['import_enum'] = True in ['enum' in p for p in model['properties']]
-
-            self.definitions.append(model)
+                model['import_enum'] = True in ['enum' in p for p in model['properties']]
+            
+            self.definitions[model['name']] = model
 
     def write_folder(self, foldername):
-        for definition in self.definitions:
+        for name, model in self.definitions.items():
             self.jinja.get_template(self.CLASS_TEMPLATE_FNAME) \
-                .stream(model=definition) \
-                .dump('{}/{}.py'.format(foldername, definition['name']))
+                .stream(model=model) \
+                .dump('{}/{}.py'.format(foldername, name))
 
 class readable_dir(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
