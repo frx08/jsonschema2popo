45cdeab0684590e1f00df26dd76dd97ef9b28625
frx08 <frx089@gmail.com>
2018-03-22 13:46:23 +0100

    process base obj, properties, improve replace $refs, allOf, as_dict()

--- jsonschema2popo.py
+++ jsonschema2popo.py
@@ -9,7 +9,6 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class JsonSchema2Popo:
     """Converts a JSON Schema to a Plain Old Python Object class"""
-    definitions = []
 
     TEMPLATES_FOLDER = 'templates/'
     CLASS_TEMPLATE_FNAME = '_class.tmpl'
@@ -26,51 +25,18 @@ class JsonSchema2Popo:
     
     def __init__(self):
         self.jinja = Environment(loader=FileSystemLoader(os.path.join(os.path.sep, SCRIPT_DIR, self.TEMPLATES_FOLDER)), trim_blocks=True)
+        
+        self.definitions = []
 
     def load(self, json_schema_file):
         self.process(json.load(json_schema_file))
 
     def process(self, json_schema):
-        # process base obj, properties, improve replace $refs, allOf...
         for _obj_name, _obj in json_schema['definitions'].items():
-            model = {}
-            model['name'] = _obj_name
-            if 'type' in _obj:
-                model['type'] = _obj['type']
-
-            model['properties'] = []
-            if 'properties' in _obj:
-                for _prop_name, _prop in _obj['properties'].items():
-                    _type = self.type_parser(_prop)
-                    _default = None
-                    if 'default' in _prop:
-                        _default = _type['type'](_prop['default'])
-                        if _type['type'] == str:
-                            _default = "'{}'".format(_default)
-
-                    _enum = None
-                    if 'enum' in _prop:
-                        _enum = _prop['enum']
-                    
-                    _format = None
-                    if 'format' in _prop:
-                        _format = _prop['format']
-                    if _type['type'] == list and 'items' in _prop and isinstance(_prop['items'], list):
-                        _format = _prop['items'][0]['format']
-                    
-                    prop = {
-                        '_name': _prop_name,
-                        '_type': _type,
-                        '_default': _default,
-                        '_enum': _enum,
-                        '_format' : _format
-                    }
-
-                    model['properties'].append(prop)
-            
+            model = self.definition_parser(_obj_name, _obj)
             self.definitions.append(model)
         
-        #oreder definition
+        # oreder definition
         changes = []
         model_names = [m['name'] for m in self.definitions]
         for model in self.definitions:
@@ -84,6 +50,50 @@ class JsonSchema2Popo:
             newindex = model_names.index(change[0])
             oldindex = model_names.index(change[1])
             self.definitions.insert(newindex, self.definitions.pop(oldindex))
+        
+        # root object
+        if 'title' in json_schema:
+            root_object_name = ''.join(x for x in json_schema['title'].title() if x.isalpha())
+        else:
+            root_object_name = 'RootObject'
+        root_model = self.definition_parser(root_object_name, json_schema)
+        self.definitions.append(root_model)
+
+    def definition_parser(self, _obj_name, _obj):
+        model = {}
+        model['name'] = _obj_name
+        if 'type' in _obj:
+            model['type'] = self.type_parser(_obj)
+
+        model['properties'] = []
+        if 'properties' in _obj:
+            for _prop_name, _prop in _obj['properties'].items():
+                _type = self.type_parser(_prop)
+                _default = None
+                if 'default' in _prop:
+                    _default = _type['type'](_prop['default'])
+                    if _type['type'] == str:
+                        _default = "'{}'".format(_default)
+
+                _enum = None
+                if 'enum' in _prop:
+                    _enum = _prop['enum']
+                
+                _format = None
+                if 'format' in _prop:
+                    _format = _prop['format']
+                if _type['type'] == list and 'items' in _prop and isinstance(_prop['items'], list):
+                    _format = _prop['items'][0]['format']
+                
+                prop = {
+                    '_name': _prop_name,
+                    '_type': _type,
+                    '_default': _default,
+                    '_enum': _enum,
+                    '_format' : _format
+                }
+                model['properties'].append(prop)
+        return model
 
     def type_parser(self, t):
         _type = None
@@ -113,6 +123,10 @@ class JsonSchema2Popo:
                 _type = self.J2P_TYPES[t['type'][0]]
             elif t['type']:
                 _type = self.J2P_TYPES[t['type']]
+        elif '$ref' in t:
+            _type = t['$ref'].split('/')[-1]
+        elif 'anyOf' in t or  'allOf' in t or  'oneOf' in t:
+            _type = list
         return { 'type': _type, 'subtype': _subtype }
 
     def write_file(self, filename):
