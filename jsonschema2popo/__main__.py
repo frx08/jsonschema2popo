b51b443e05bf505d6f0492345794ef4dab959336
frx08 <frx089@gmail.com>
2018-04-06 12:01:20 +0200

    fix package setup

---
+++ jsonschema2popo/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+
+import os
+import sys
+
+if __package__ == '':
+    path = os.path.dirname(os.path.dirname(__file__))
+    sys.path.insert(0, path)
+
+from jsonschema2popo.jsonschema2popo import main as _main  # noqa
+
+if __name__ == '__main__':
+    sys.exit(_main())
